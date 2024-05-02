ba9e04e65f8c9c3cb46911cd43341b53933f24e6
ifeoluwa02 <88455435+ifeoluwa02@users.noreply.github.com>
2024-05-02 13:53:30 +0100

    Update server.js

--- currency-converter/backend/server.js
+++ currency-converter/backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.post('/api/convert',cors(), (req,res)=>{
     // What ever function u want the backend to do and the actual thing u want it to return if there's something to return will be spent like this
     const {  amount } = req.body;
-    console.log(amount)
+    // console.log(amount)
     const convertedAmount = amount * 2;
 
    res.status(200).json({ convertedAmount })
